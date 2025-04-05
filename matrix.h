2c8e425e929525556b068b73f8d43eeeab037d05
vitaly-castLabs <83583780+vitaly-castLabs@users.noreply.github.com>
2025-04-05 21:57:30 +0700

    matrix to std::cout / cerr with <<

--- matrix.h
+++ matrix.h
@@ -2,6 +2,7 @@
 #define ECCPP_MATRIX_H
 
 #include <vector>
+#include <iostream>
 
 namespace eccpp {
 
@@ -20,6 +21,19 @@ public:
     
     const std::vector<T>& operator[](size_t i) const { return data[i]; }
     std::vector<T>& operator[](size_t i) { return data[i]; }
+
+    friend std::ostream& operator<<(std::ostream& os, const matrix<T>& mat) {
+        for (size_t i = 0; i < mat.rows(); ++i) {
+            os << "[";
+            for (size_t j = 0; j < mat.cols(); ++j) {
+                os << mat[i][j];
+                if (j < mat.cols() - 1)
+                    os << ", ";
+            }
+            os << "]\n";
+        }
+        return os;
+    }
 };
 
 }
