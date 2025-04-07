235a69be48df488b008c46ab2a397f2a85a97a3d
vitaly-castLabs <83583780+vitaly-castLabs@users.noreply.github.com>
2025-04-07 13:39:12 +0700

    Kronecker product updated to match new matrix template

--- kron.h
+++ kron.h
@@ -7,25 +7,23 @@
 
 namespace eccpp {
 
-template <typename T>
-matrix<T> kron(const matrix<T>& A, const matrix<T>& B) {
-    size_t a_row = A.rows();
-    size_t a_col = A.cols();
-    size_t b_row = B.rows();
-    size_t b_col = B.cols();
-    
-    matrix<T> result(a_row * b_row, a_col * b_col);
-    
-    for (size_t i = 0; i < a_row; ++i) {
-        for (size_t j = 0; j < a_col; ++j) {
-            T a_val = A[i][j];
-            for (size_t bi = 0; bi < b_row; ++bi) {
-                for (size_t bj = 0; bj < b_col; ++bj)
-                    result[i * b_row + bi][j * b_col + bj] = a_val * B[bi][bj];
-            }
+template <typename T, size_t Rows1, size_t Cols1, size_t Rows2, size_t Cols2>
+matrix<T, Rows1 * Rows2, Cols1 * Cols2> kron(
+    const matrix<T, Rows1, Cols1>& m1,
+    const matrix<T, Rows2, Cols2>& m2) 
+{
+    matrix<T, Rows1 * Rows2, Cols1 * Cols2> result;
+
+    for (size_t r1 = 0; r1 < Rows1; ++r1) {
+        for (size_t c1 = 0; c1 < Cols1; ++c1) {
+            const T val = m1[r1][c1];
+
+            for (size_t r2 = 0; r2 < Rows2; ++r2)
+                for (size_t c2 = 0; c2 < Cols2; ++c2)
+                    result[r1 * Rows2 + r2][c1 * Cols2 + c2] = val * m2[r2][c2];
         }
     }
-    
+
     return result;
 }
 
