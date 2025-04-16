93330481e5ac35807b2553e848eb85a076f0f967
Vitaly Ivanov <vitaly.ivanov@castlabs.com>
2025-04-16 16:45:22 +0700

    C++ min* matching precisely reference implementation in MATLAB

--- minstar.h
+++ minstar.h
@@ -8,21 +8,23 @@
 
 namespace eccpp {
 
-// this might be needed to match MATLAB behavior precisely:
-// it seems to convert negative zero to just 0.0, however I have
-// no idea if it matters at all
-/*
-static double matlab_fix_negative_zero(double val) {
-    if (std::fpclassify(val) == FP_ZERO)
+// there's a bunch of subtle differences between C++'s std::copysign() and MATLAB's
+// sign() around handling zeros and NaNs
+static double matlab_sign(double a) {
+    const auto a_type = std::fpclassify(a);
+    // MATLAB's sign(0) and sign(-0) both return 0
+    if (a_type == FP_ZERO)
         return 0;
 
-    return val;
+    if (a_type == FP_NAN)
+        return std::numeric_limits<double>::quiet_NaN();
+
+    return a > 0 ? 1 : -1;
 }
-*/
 
 double minstar(double a, double b, bool approx) {
     if (approx || std::isinf(a) || std::isinf(b))
-        return std::copysign(1, a) * std::copysign(1, b) * std::min(std::abs(a), std::abs(b));
+        return matlab_sign(a) * matlab_sign(b) * std::min(std::abs(a), std::abs(b));
 
     return 2 * std::atanh(std::tanh(a / 2) * std::tanh(b / 2));
 }
