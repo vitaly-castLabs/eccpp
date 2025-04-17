a1199144215aa5a84b07682d46012f85922510d9
Vitaly Ivanov <vitaly.ivanov@castlabs.com>
2025-04-17 15:44:56 +0700

    MATLAB sign() moved to separate file (it's used by phi and such)

--- minstar.h
+++ minstar.h
@@ -4,27 +4,14 @@
 #define ECCPP_MINSTAR_H
 
 #include <cmath>
-#include <algorithm>
+
+#include "./sign.h"
 
 namespace eccpp {
 
-// there's a bunch of subtle differences between C++'s std::copysign() and MATLAB's
-// sign() around handling zeros and NaNs
-static double matlab_sign(double a) {
-    const auto a_type = std::fpclassify(a);
-    // MATLAB's sign(0) and sign(-0) both return 0
-    if (a_type == FP_ZERO)
-        return 0;
-
-    if (a_type == FP_NAN)
-        return std::numeric_limits<double>::quiet_NaN();
-
-    return a > 0 ? 1 : -1;
-}
-
 double minstar(double a, double b, bool approx) {
     if (approx || std::isinf(a) || std::isinf(b))
-        return matlab_sign(a) * matlab_sign(b) * std::min(std::abs(a), std::abs(b));
+        return sign(a) * sign(b) * std::min(std::abs(a), std::abs(b));
 
     return 2 * std::atanh(std::tanh(a / 2) * std::tanh(b / 2));
 }
---
+++ sign.h
@@ -0,0 +1,26 @@
+// MATLAB sign function
+
+#ifndef ECCPP_SIGN_H
+#define ECCPP_SIGN_H
+
+#include <cmath>
+
+namespace eccpp {
+
+// there's a bunch of subtle differences between C++'s std::copysign() and MATLAB's
+// sign() around handling zeros and NaNs
+static double sign(double a) {
+    const auto a_type = std::fpclassify(a);
+    // MATLAB's sign(0) and sign(-0) both return 0
+    if (a_type == FP_ZERO)
+        return 0;
+
+    if (a_type == FP_NAN)
+        return std::numeric_limits<double>::quiet_NaN();
+
+    return a > 0 ? 1 : -1;
+}
+
+} // namespace eccpp
+
+#endif // ECCPP_SIGN_H
