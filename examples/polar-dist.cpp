a200b507d68beac37cae013be7fb1a28b531c950
Vitaly Ivanov <vitaly.ivanov@castlabs.com>
2025-05-09 23:08:26 +0700

    longest common run tracking, CW column shuffling (in order to reduce long common runs)

--- examples/polar-dist.cpp
+++ examples/polar-dist.cpp
@@ -11,10 +11,12 @@
 #include "gn.h"
 #include "polar-enc.h"
 #include "hamdist.h"
+#include "longest-common.h"
+#include "shuffle.h"
 
 //#define N 8192
-#define N 2048
-//#define N 256
+//#define N 2048
+#define N 256
 
 std::string chronoToHms(std::chrono::duration<int, std::milli> duration) {
     const int total_ms = duration.count();
@@ -36,7 +38,6 @@ std::string chronoToHms(std::chrono::duration<int, std::milli> duration) {
 
 int main() {
     eccpp::polar_enc_butterfly enc(N);
-    size_t min_dist = N + 1;
 
     // these indices are produced with frozen-bits.cpp
     const std::vector<size_t> info_bits =
@@ -51,43 +52,68 @@ int main() {
         {255, 254, 253, 251, 247, 239, 223, 191, 127};
 #endif
 
-    std::vector<int> msg(N);
-    const size_t codeword_count = 1 << info_bits.size();
-    std::vector<std::vector<int>> codewords;
-    codewords.reserve(codeword_count);
+    // unshuffled + shuffled with different seeds
+    const int num_iter = 1 + 10;
+    std::uint_fast32_t shuffle_seed = 3;
+    size_t best_max_lc_run = N + 1;
+    for (int iter = 0; iter < num_iter; ++iter) {
+        size_t min_dist = N + 1;
+        size_t max_lc_run = 0;
 
-    std::cout << "\nGenerating codewords, might take a while...\n\n";
-    const auto start = std::chrono::steady_clock::now();
-    while (codewords.size() < codeword_count) {
-        const auto codeword = enc.encode(msg);
-        for (const auto& cw: codewords) {
-            const auto dist = eccpp::hamdist(cw, codeword);
-            if (dist == 0)
-                throw std::runtime_error("Hamming distance is 0 - identical codewords");
+        std::vector<int> msg(N);
+        const size_t codeword_count = 1 << info_bits.size();
+        std::vector<std::vector<int>> codewords;
+        codewords.reserve(codeword_count);
 
-            min_dist = std::min(min_dist, dist);
+        std::cout << "\n--- " << (iter + 1) << "/" << num_iter << ": " << (iter ? "Shuffled" : "Original") << " polar codes -----------------------------\n\n";
+        if (iter) {
+            shuffle_seed = shuffle_seed * 3 + 11;
+            std::cout << "Shuffle seed: " << shuffle_seed << "\n";
         }
-        codewords.push_back(codeword);
+        std::cout << "Generating codewords, might take a while...\n\n";
+        const auto start = std::chrono::steady_clock::now();
+        while (codewords.size() < codeword_count) {
+            auto codeword = enc.encode(msg);
+            if (iter)
+                eccpp::shuffle(codeword, shuffle_seed);
 
-        // increment message
-        for (size_t i = 0; i < info_bits.size(); ++i) {
-            auto& bit = msg[info_bits[i]];
-            bit ^= 1;
+            for (const auto& cw: codewords) {
+                const auto dist = eccpp::hamdist(cw, codeword);
+                if (dist == 0)
+                    throw std::runtime_error("Hamming distance is 0 - identical codewords");
 
-            if (bit) {
-                for (size_t j = 0; j < i; ++j)
-                    msg[info_bits[j]] = 0;
+                min_dist = std::min(min_dist, dist);
 
-                break;
+                const auto lcr = eccpp::lc_aligned_run<int, size_t>(cw, codeword);
+                max_lc_run = std::max(max_lc_run, lcr);
             }
+            codewords.push_back(codeword);
+
+            // increment message
+            for (size_t i = 0; i < info_bits.size(); ++i) {
+                auto& bit = msg[info_bits[i]];
+                bit ^= 1;
+
+                if (bit) {
+                    for (size_t j = 0; j < i; ++j)
+                        msg[info_bits[j]] = 0;
+
+                    break;
+                }
+            }
+
+            std::cout << "\rProcessed " << codewords.size() << " of " << codeword_count << " codewords (min dist: " << min_dist << ")     ";
         }
+        const auto end = std::chrono::steady_clock::now();
+        const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << "\n\nDone in " << chronoToHms(elapsed) << "\n";
 
-        std::cout << "\rProcessed " << codewords.size() << " of " << codeword_count << " codewords (min dist: " << min_dist << ")     ";
+        std::cout << "\nMinimum Hamming distance:     " << min_dist << "\n";
+        std::cout << "Longest common run:           " << max_lc_run << "\n";
+        std::cout << "Minimum repeat code distance: " << N / info_bits.size() << "\n";
+
+        best_max_lc_run = std::min(best_max_lc_run, max_lc_run);
     }
-    const auto end = std::chrono::steady_clock::now();
-    const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "\n\nDone in " << chronoToHms(elapsed) << "\n";
 
-    std::cout << "\nMinimum Hamming distance:     " << min_dist << "\n";
-    std::cout << "Minimum repeat code distance: " << N / info_bits.size() << "\n";
+    std::cout << "\n\nBest longest common run: " << best_max_lc_run << "\n";
 }
