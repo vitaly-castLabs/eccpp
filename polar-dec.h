06e9aabd97084b79acccba6304a8fff675522a4c
Vitaly Ivanov <vitaly.ivanov@castlabs.com>
2025-05-18 19:12:24 +0700

    polar decoding with correlation search

--- polar-dec.h
+++ polar-dec.h
@@ -72,6 +72,61 @@ public:
         return dec_result;
     }
 
+    // same as before, but we have only partial LLR array and we don't know the offset/position,
+    // so we need to conduct correlation search as well
+    result decode_unaligned(const std::vector<T>& llr, const std::vector<size_t>& info_bits) const {
+        if (llr.size() > n_)
+            throw std::invalid_argument("LLR size must not exceed transform size");
+
+        if (info_bits.empty())
+            throw std::invalid_argument("Info bits must not be empty");
+
+        if (info_bits.size() > n_)
+            throw std::invalid_argument("Info bits size greater than transform size");
+
+        result dec_result;
+        dec_result.msg.resize(info_bits.size());
+
+        // encode all possible messages and pick the best match
+        T best_match = std::numeric_limits<T>::lowest();
+        T second_best_match = best_match;
+        polar_enc_butterfly enc(n_);
+        std::vector<int> msg_with_frozen_bits(n_);
+        const auto max_offset = n_ - llr.size();
+        while (true) {
+            const auto codeword = enc.encode(msg_with_frozen_bits);
+            for (auto off = 0; off <= max_offset; ++off) {
+                T match = 0;
+                for (size_t i = 0; i < n_; ++i) {
+                    const auto& prob = llr[i + off];
+                    match += codeword[i] ? -prob : prob;
+                }
+
+                if (match > best_match) {
+                    second_best_match = best_match;
+                    best_match = match;
+                    extract_message(dec_result.msg, msg_with_frozen_bits, info_bits);
+                }
+                else if (match > second_best_match)
+                    second_best_match = match;
+            }
+
+            if (!next_message(msg_with_frozen_bits, info_bits))
+                break;
+        }
+
+        T llr_sum = 0;
+        for (auto v: llr)
+            llr_sum += std::abs(v);
+
+        // ideally the best match should be close to sum(abs(llr)) and the runner-up should be
+        // significantly lower - if both are true, then we can be confident we didn't pick up
+        // a random value
+        dec_result.confidence = best_match * (best_match - second_best_match) / (llr_sum * llr_sum);
+        return dec_result;
+    }
+
+
 private:
     // increment message bits (f are frozen bits): ff0f0 -> ff0f1 -> ff1f0 -> ff1f1. Returns
     // false (no more messages) at 11..1 -> 00..0 roll-over.
